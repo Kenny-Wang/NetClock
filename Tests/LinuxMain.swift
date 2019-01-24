8f01e6a1e282f668fd984d9f7e4555e939f3c6cd
Gavin Eadie <gavin@umich.edu>
2019-01-24 16:00:38 -0500

    Initial check in ..

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import NetClockTests
+
+XCTMain([
+    testCase(NetClockTests.allTests),
+])
---
+++ Tests/NetClockTests/NetClockTests.swift
@@ -0,0 +1,23 @@
+//
+//  NetClockTests.swift
+//  Ramsay Consulting
+//
+//  Created by Gavin Eadie on 1/24/19.
+//  Copyright © 2019 Ramsay Consulting. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import NetClock
+
+class NetClockTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(NetClock().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
